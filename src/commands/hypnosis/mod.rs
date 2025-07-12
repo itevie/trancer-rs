87b90729f942c071082ac38e0c60de09d0e006f1
Isabella <izzyplays07@gmail.com>
2025-07-12 20:43:45 +0100

    New command: .removetrigger

--- src/commands/hypnosis/mod.rs
+++ src/commands/hypnosis/mod.rs
@@ -1,7 +1,8 @@
 mod trigger_send;
 mod triggers_add;
+mod triggers_remove;
 mod triggers_view;
 
 use crate::cmd_import_map;
 
-cmd_import_map!(trigger_send, triggers_view, triggers_add);
+cmd_import_map!(trigger_send, triggers_view, triggers_add, triggers_remove);
---
+++ src/commands/hypnosis/triggers_remove.rs
@@ -0,0 +1,54 @@
+use crate::cmd_util::arg_parser::{CommandArgumentStruct, PCACV};
+use crate::cmd_util::args::{ArgType, Argument, StringArgTypeFlag, TrancerArguments};
+use crate::cmd_util::types::TrancerCommandType;
+use crate::cmd_util::CommandTrait;
+use crate::cmd_util::{trancer_handler, TrancerDetails};
+use crate::cmd_util::{ArgumentError, TrancerCommand, TrancerError, TrancerResponseType};
+use crate::commands::CommandHasNoArgs;
+use crate::models::user_imposition::UserImposition;
+use crate::util::lang::warn;
+use crate::{command_argument_struct, command_file, reply};
+use serenity::all::{
+    ButtonStyle, CreateActionRow, CreateButton, CreateInteractionResponse,
+    CreateInteractionResponseMessage, EditMessage,
+};
+use serenity::builder::CreateMessage;
+use serenity::futures::StreamExt;
+use std::collections::HashMap;
+use std::time::Duration;
+use tracing::error;
+
+command_argument_struct!(AddTriggerArgs {
+    what: String, PCACV::String
+});
+
+command_file! {
+    TrancerCommand::<AddTriggerArgs> {
+        name: "removetrigger".to_string(),
+        t: TrancerCommandType::Help,
+        description: "Remove a trigger!".to_string(),
+        details: TrancerDetails {
+            arguments: Some(TrancerArguments {
+                required: 1,
+                args: vec![Argument {
+                    name: "what".to_string(),
+                    t: ArgType::String {
+                        flags: Some(vec![StringArgTypeFlag::TakeContent])
+                    },
+                    details: Default::default()
+                }],
+            }),
+            ..Default::default()
+        },
+
+        handler: trancer_handler!(|ctx, args| {
+            if !UserImposition::has(&ctx.sy, ctx.msg.author.id, args.what.clone()).await? {
+                return Ok(TrancerResponseType::Content(warn("You do not have that trigger!")));
+            }
+
+            UserImposition::remove(&ctx.sy, ctx.msg.author.id, args.what.clone()).await?;
+
+            Ok(TrancerResponseType::Content("Removed that trigger!".to_string()))
+        }),
+    }
+}
