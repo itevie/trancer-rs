74e1e01c03986e71aa4fe38b91b73782f6b4c6e0
Isabella <izzyplays07@gmail.com>
2025-09-13 23:52:56 +0100

    add card ideas, hi nick

--- src/models/card.rs
+++ src/models/card.rs
@@ -5,7 +5,7 @@ enum_with_sql!(Rarity {
     Uncommon = "uncommon",
     Rare = "rare",
     Epic = "epic",
-    Legendary = "legendary"
+    Mythic = "mythic"
 });
 
 impl_from_row!(Card, CardField {
@@ -18,3 +18,14 @@ impl_from_row!(Card, CardField {
     rarity: String,
     created_at: String
 });
+
+// Ideas for cards
+// - Remove rarity from cards
+// - Each card has the set of rarities
+// - Make aquired_cards have a rarity
+// - So like, legendary dawn, common dawn etc.
+// - For the existing aquired cards, give them the same rarity that card is but as aquired_cards
+//   So like, if they have 2 Dawns, they'd get 2 mythic dawns
+// - They could be listed like Dawn [2x common 1x rare], but with emojis
+// - Also store the image files in the cloudflare bucket
+// - People that are level 10 or whatever can make their own card without asking
