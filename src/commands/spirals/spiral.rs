6e15720c36a2df5c16e9b41cd90a9795b8c92499
Isabella <izzyplays07@gmail.com>
2026-05-05 15:44:44 +0100

    Added buttons for favouriting a spiral

--- src/commands/spirals/spiral.rs
+++ src/commands/spirals/spiral.rs
@@ -4,7 +4,14 @@ use crate::cmd_util::{trancer_handler, TrancerDetails};
 use crate::cmd_util::{TrancerCommand, TrancerResponseType};
 use crate::command_file;
 use crate::commands::CommandHasNoArgs;
+use crate::models::favourite_spiral::FavouriteSpiral;
 use crate::models::spiral::Spiral;
+use serenity::all::{
+    ButtonStyle, CreateInteractionResponse, CreateInteractionResponseMessage, CreateMessage,
+};
+use serenity::builder::{CreateActionRow, CreateButton};
+use serenity::collector::ComponentInteractionCollector;
+use std::time::Duration;
 
 command_file! {
     TrancerCommand::<CommandHasNoArgs> {
@@ -15,10 +22,65 @@ command_file! {
             aliases: Some(vec!["s".to_string()]),
             ..Default::default()
         },
-
         handler: trancer_handler!(|ctx, _args| {
             let spiral = Spiral::get_random(&ctx.sy).await?;
-            Ok(TrancerResponseType::Content(spiral.link))
+
+            let fav_id = format!("spiral_fav:{}", spiral.id);
+            let info_id = format!("spiral_info:{}", spiral.id);
+
+            // Send message with buttons
+            let msg = ctx.msg.channel_id.send_message(&ctx.sy.http, CreateMessage::new().content(&spiral.link)
+                .components(
+                    vec![CreateActionRow::Buttons(vec![
+                         CreateButton::new(&fav_id)
+                            .emoji('⭐')
+                            .style(ButtonStyle::Primary),
+
+                        CreateButton::new(&info_id)
+                            .emoji('ℹ')
+                            .style(ButtonStyle::Secondary),
+                    ])]
+                )
+            ).await?;
+
+            // Listen for button clicks (only for this message)
+            while let Some(interaction) = ComponentInteractionCollector::new(&ctx.sy)
+                .message_id(msg.id)
+                .timeout(Duration::from_secs(60))
+                .await
+            {
+                let custom_id = &interaction.data.custom_id;
+
+                if let Some(id) = custom_id.strip_prefix("spiral_fav:") {
+                    if FavouriteSpiral::exists(&ctx.sy, interaction.user.id, spiral.id).await? {
+                        interaction.create_response(
+                            &ctx.sy.http,
+                            CreateInteractionResponse::Message(
+                                CreateInteractionResponseMessage::new()
+                                    .content("You have already favourited this spiral!".to_string())
+                                    .ephemeral(true),
+                            )
+                        ).await?;
+                        continue;
+                    }
+
+                    FavouriteSpiral::add(&ctx.sy, interaction.user.id, spiral.id).await?;
+                     interaction.create_response(
+                            &ctx.sy.http,
+                            CreateInteractionResponse::Message(
+                                CreateInteractionResponseMessage::new()
+                                    .content("Added to favourites!".to_string())
+                                    .ephemeral(true),
+                            )
+                        ).await?;
+                }
+
+                if let Some(id) = custom_id.strip_prefix("spiral_info:") {
+
+                }
+            }
+
+            Ok(TrancerResponseType::None)
         }),
     }
 }
